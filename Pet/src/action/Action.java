595dd13029d0d45b8817464e2c769586509d6255
wjrdjs005 <wjrdjs005@naver.com>
2022-01-27 13:53:54 +0900

    project init

---
+++ Pet/src/action/Action.java
@@ -0,0 +1,9 @@
+package action;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Action {
+	public ActionForward execute(HttpServletRequest req, HttpServletResponse res) throws ;
+}
