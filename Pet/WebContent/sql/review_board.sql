2068f2e3f8e1bea3662df58fea3e794d3de45c6e
나리아름 <@DESKTOP-SOMABG9>
2022-02-10 17:56:32 +0900

    0210

--- Pet/WebContent/sql/review_board.sql
+++ Pet/WebContent/sql/review_board.sql
@@ -9,12 +9,14 @@ CREATE TABLE review_board(
    rb_text     varchar2(300),  --내용
    rb_date date default sysdate,      --글의 작성 날짜
    rb_redate date default sysdate,    --글의 수정 날짜
+   foreign key(hotel_no) references hotel(hotel_no) on delete cascade,
    PRIMARY KEY(review_no)
    );
    
+   
    --테스트 데이터 insert
    insert into review_board
-   values(1,1,'admin','후기입니다.','좋아요',sysdate,null)
+   values('1',1,'admin','후기입니다.','좋아요',sysdate,null)
    
    insert into review_board
    values(2,2,'admin','후기입니다2.','좋아요2',sysdate,null)
