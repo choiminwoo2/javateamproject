3a3271ea8cf8294158199d89f11c225743b14fbc
wjrdjs005 <wjrdjs005@naver.com>
2022-02-11 16:46:16 +0900

    commit

--- Pet/WebContent/sql/hotelSQL.sql
+++ Pet/WebContent/sql/hotelSQL.sql
@@ -3,7 +3,7 @@ DROP TABLE HOTEL cascade constraints purge;
 --테이블 생성
 CREATE TABLE HOTEL(
 	hotel_no number primary key,
-	hotel_name varchar2(20) not null,
+	hotel_name varchar2(50) not null,
 	hotel_animal_grade number(3) check(hotel_animal_grade in(0,1,2)),
 	hotel_price_5kglt number(6) not null,
 	hotel_price_5ge8lt number(6) not null,
@@ -23,13 +23,32 @@ SELECT * FROM HOTEL;
 CREATE SEQUENCE hotel_seq;
 
 --더미데이터
---트리거 
-CREATE OR REPLACE TRIGGER DELETE_HOTEL
-	AFTER DELETE ON USERDATA
-	FOR EACH ROW
-BEGIN
-	DELETE HOTEL 
-	WHERE HOTEL_ID = :OLD.USERDATA.ID
-END;
-/
+insert into hotel values(1,"애견사랑", 0, ,30000,50000,70000,90000,"02-514-7899","512","경기 파주시
+ 새꽃로 1","하루빌딩 1층 403호","test.jpg","hoteluse");
+insert into hotel values(2,"모든펫을 아껴주는", 2, ,50000,70000,90000,110000,"02-514-7899","512","경기 파주시
+ 새꽃로 1","하루빌딩 1층 403호","test.jpg","hoteluse");
+ insert into hotel values(3,"고양이나라", 1, ,30000,50000,70000,90000,"02-514-7899","512","경기 파주시
+ 새꽃로 1","하루빌딩 1층 403호","test.jpg","hoteluse");
+ insert into hotel values(4,"당신의 강아지", 0, ,30000,50000,70000,90000,"02-514-7899","512","경기 파주시
+ 새꽃로 1","하루빌딩 1층 403호","test.jpg","hoteluse");
+ insert into hotel values(5,"고양이 집사", 1, ,30000,50000,70000,90000,"02-514-7899","512","경기 파주시
+ 새꽃로 1","하루빌딩 1층 403호","test.jpg","hoteluse");
+ insert into hotel values(6,"테스트", 0, ,30000,50000,70000,90000,"02-514-7899","512","서울 용산구
+ 새꽃로 1","하루빌딩 1층 403호","test.jpg","hoteluse");
+  insert into hotel values(7,"TEST", 2, ,30000,50000,70000,90000,"02-514-7899","512","서울 용산구
+ 새꽃로 1","하루빌딩 1층 403호","test.jpg","hoteluse");
+  insert into hotel values(8,"애견스 닷컴", 0, ,30000,50000,70000,90000,"02-514-7899","512","서울 용산구
+ 새꽃로 1","하루빌딩 1층 403호","test.jpg","hoteluse");
+  insert into hotel values(9,"우리호텔", 2, ,30000,50000,70000,90000,"02-514-7899","512","서울 용산구
+ 새꽃로 1","하루빌딩 1층 403호","test.jpg","hoteluse");
+  insert into hotel values(10,"냥냥", 1, ,30000,50000,70000,90000,"02-514-7899","512","서울 용산구
+ 새꽃로 1","하루빌딩 1층 403호","test.jpg","hoteluse");
+  insert into hotel values(11,"고양이 마을", 1, ,30000,50000,70000,90000,"02-514-7899","512","서울 용산구
+ 새꽃로 1","하루빌딩 1층 403호","test.jpg","hoteluse");
+   insert into hotel values(11,"고양이 마을", 1, ,70000,90000,100000,140000,"02-514-7899","512","서울 용산구
+ 새꽃로 1","하루빌딩 1층 403호","test.jpg","hoteluse");
+    insert into hotel values(11,"고양이 마을", 1, ,70000,90000,100000,140000,"02-514-7899","512","서울 용산구
+ 새꽃로 1","하루빌딩 1층 403호","test.jpg","hoteluse");
+
+
 	
